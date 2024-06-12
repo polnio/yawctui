746df1ae86955455e7bc7b9d0361132079bad46b
Po Co <polniolino@gmail.com>
2024-06-12 17:56:58 +0200

    Setup project

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Yet Another Web Client (Terminal User Interface)";
+
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs;
+          mkShell {
+            # buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            buildInputs = [ rustup pre-commit ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+}
